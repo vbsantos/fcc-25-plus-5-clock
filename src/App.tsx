ea8c654d28616ccabc5330806fbaa47b9b6894e0
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-02-19 17:32:48 -0300

    v1 - All tests passed, add style and alarm

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,134 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react';
+
+import "./style.css"
 
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const errorId = useRef<number | null>(null);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const [breakLength, setBreakLength] = useState<number>(5);
+  const [sessionLength, setSessionLength] = useState<number>(25);
+  const [timerLabel, setTimerLabel] = useState<string>("Session");
+  const [timeLeft, setTimeLeft] = useState<number>(sessionLength * 60);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (error) {
+      if (errorId.current) {
+        clearTimeout(errorId.current);
+      }
+      errorId.current = setTimeout(() => setError(false), 1000);
+    }
+  }, [error])
+
+  useEffect(() => {
+    let intervalId: number;
+    if (isRunning) {
+      if (timeLeft === 0) {
+        audioRef.current?.play()
+      }
+      if (timeLeft >= 0) {
+        intervalId = setInterval(() => {
+          setTimeLeft(timeLeft - 1);
+        }, 1000);
+      } else {
+        setTimerLabel(timerLabel === "Session" ? "Break" : "Session");
+        setTimeLeft(timerLabel === "Session" ? breakLength * 60 : sessionLength * 60);
+      }
+      document.title = `[${timerLabel}] ${formatTime(timeLeft)}`
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, timeLeft, breakLength, sessionLength, timerLabel]);
+
+  const handleError = () => {
+    setError(true);
+    if (errorId.current) {
+      clearTimeout(errorId.current);
+    }
+    errorId.current = setTimeout(() => setError(false), 1000);
+  }
+
+  const handleBreakUpdate = (diff: number) => {
+    if (!isRunning) {
+      if ((breakLength === 60 && diff > 0) || (breakLength === 1 && diff < 0)) {
+        handleError();
+        return;
+      }
+      const time = breakLength + diff;
+      setBreakLength(time);
+    }
+  }
+
+  const handleSessionUpdate = (diff: number) => {
+    if (!isRunning) {
+      if ((sessionLength === 60 && diff > 0) || (sessionLength === 1 && diff < 0)) {
+        handleError();
+        return;
+      }
+      const time = sessionLength + diff;
+      setSessionLength(time);
+      setTimeLeft(time * 60)
+    }
+  }
+
+  const handleStartStop = () => {
+    setIsRunning((_isRunning) => !_isRunning);
+  };
+
+  const handleReset = () => {
+    document.title = "25 + 5 Clock";
+
+    setIsRunning(false);
+    setTimerLabel("Session");
+    setTimeLeft(25 * 60);
+    setSessionLength(25);
+    setBreakLength(5);
+
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+  };
+
+  const formatTime = (time: number) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  };
 
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <div id="main-container" className={error ? "error" : (timerLabel === "Session" ? "" : "break")}>
+      <div id="pomodoro">
+        <h1 id="timer-label">{error ? "ERROR" : timerLabel}</h1>
+        <div id="time-left">{formatTime(timeLeft)}</div>
 
-export default App
+        <button id="start_stop" onClick={handleStartStop}>{isRunning ? "Stop" : "Start"}</button>
+        <button id="reset" onClick={handleReset}>Reset</button>
+        <audio id="beep" ref={audioRef} src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" />
+
+        <div id="configs">
+
+          <div className="config">
+            <h2 id="break-label">Break Length</h2>
+            <div className="number-selector">
+              <button id="break-decrement" disabled={isRunning} onClick={() => handleBreakUpdate(-1)}>-</button>
+              <div id="break-length">{breakLength}</div>
+              <button id="break-increment" disabled={isRunning} onClick={() => handleBreakUpdate(+1)}>+</button>
+            </div>
+          </div>
+
+          <div className="config">
+            <h2 id="session-label">Session Length</h2>
+            <div className="number-selector">
+              <button id="session-decrement" disabled={isRunning} onClick={() => handleSessionUpdate(-1)}>-</button>
+              <div id="session-length">{sessionLength}</div>
+              <button id="session-increment" disabled={isRunning} onClick={() => handleSessionUpdate(+1)}>+</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
